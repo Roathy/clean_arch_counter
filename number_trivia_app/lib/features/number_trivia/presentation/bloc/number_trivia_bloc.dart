c10121ec2a45ae5be01104bd740ef6129da30e85
Julio Vergara <gogczoro@gmail.com>
2024-04-09 16:57:54 -0600

    Implement Failure class and RandomNumber

--- number_trivia_app/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ number_trivia_app/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -4,6 +4,7 @@ import 'package:equatable/equatable.dart';
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
+// TODO
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   NumberTriviaBloc() : super(NumberTriviaInitial()) {
     on<NumberTriviaEvent>((event, emit) {});
--- number_trivia_app/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ number_trivia_app/test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -3,33 +3,34 @@ import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:number_trivia_app/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:number_trivia_app/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:number_trivia_app/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia_app/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
 class MockNumberTriviaRepository extends Mock implements NumberTriviaRepository {}
 
 void main() {
-  late final GetConcreteNumberTrivia usecase;
+  late final GetRandomNumberTrivia usecase;
   late final MockNumberTriviaRepository mockNumberTriviaRepository;
 
   setUp(() {
     mockNumberTriviaRepository = MockNumberTriviaRepository();
-    usecase = GetConcreteNumberTrivia(
-      repository: mockNumberTriviaRepository,
-    );
+    usecase = GetRandomNumberTrivia(mockNumberTriviaRepository);
   });
 
-  const testNumber = 1;
-  const testNumberTrivia = NumberTrivia(number: 1, text: 'test');
-
   test('should get trivia for the number from the repository', () async {
+    // número para probar el ConcreteNumberTrivia
+    const tNumber = 1;
+    // esto va a ser retornado por la clase Mock
+    const tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+
     // arrange
-    when(mockNumberTriviaRepository.getConcreteNumberTrivia(any)).thenAnswer((_) async => const Right(testNumberTrivia));
+    when(mockNumberTriviaRepository.getConcreteNumberTrivia(any)).thenAnswer((_) async => const Right(tNumberTrivia));
     // act
-    final result = await usecase(number: testNumber);
+    final result = await usecase(const Params(number: tNumber));
+
     // assert
-    expect(result, const Right(testNumberTrivia));
-    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(testNumber));
+    expect(result, const Right(tNumberTrivia));
+    verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
     verifyNoMoreInteractions(mockNumberTriviaRepository);
   });
 }
